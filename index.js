e6e75e76d4119ea358e9b03316581f5637561661
stasyukhymenko <yukhymenko.stas@gmail.com>
2024-06-05 00:31:25 +0300

    add some regexps

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 
 function markdownToHtml(md) {
+    md = md.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+    md = md.replace(/_(.*?)_/g, '<i>$1</i>');
+    md = md.replace(/`(.*?)`/g, '<tt>$1</tt>');
+    md = md.replace(/```([\s\S]*?)```/g, '<pre>$1</pre>');
+    md = md.split('\n\n').map(para => `<p>${para.replace(/\n/g, ' ')}</p>`).join('\n');
+
+    return md;
+}
+function markdownToHtml(md) {
     //TODO
 }
 
