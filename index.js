f801a2fdb32b3f6067b5bdc515369ecc9d0a68cb
stasyukhymenko <yukhymenko.stas@gmail.com>
2024-06-05 20:46:19 +0300

    Fix some mistakes with --out

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ function markdownToHtml(md) {
     md = md.replace(/`(.*?)`/g, '<tt>$1</tt>');
     md = md.replace(/```([\s\S]*?)```/g, '<pre>$1</pre>');
     md = md.split('\n\n').map(para => `<p>${para.replace(/\n/g, ' ')}</p>`).join('\n');
-
     return md;
 }
 
 const args = process.argv.slice(2);
 
 const outputIndex = args.indexOf('--out');
-
-const inputFilePath = outputIndex >= 0 ? args[outputIndex + 1] : args[0];
+const inputFilePath = outputIndex >= 0 ? args[0] : args[0];
 
 if (!inputFilePath) {
     console.error('Please provide an input markdown file.');
@@ -30,8 +28,6 @@ fs.readFile(inputFilePath, 'utf8', (err, data) => {
     const html = markdownToHtml(data.replace(/(^|\n)```([\s\S]*?)```(\n|$)/g, '$1<pre>$2</pre>$3'));
 
     if (outputIndex >= 0) {
-        console.log(html);
-    } else {
         fs.writeFile('output.html', html, 'utf8', err => {
             if (err) {
                 console.error('Error writing to output file:', err);
@@ -39,5 +35,7 @@ fs.readFile(inputFilePath, 'utf8', (err, data) => {
             }
             console.log('HTML saved to output.html');
         });
+    } else {
+        console.log(html);
     }
 });
