8d65573590f1f164865a4e9d8d026b09b2875da0
stasyukhymenko <yukhymenko.stas@gmail.com>
2024-06-05 23:39:38 +0300

    refactor code

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 const fs = require('fs');
-
-function markdownToHtml(md) {
-    md = md.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
-    md = md.replace(/_(.*?)_/g, '<i>$1</i>');
-    md = md.replace(/`(.*?)`/g, '<tt>$1</tt>');
-    md = md.replace(/```([\s\S]*?)```/g, '<pre>$1</pre>');
-    md = md.split('\n\n').map(para => `<p>${para.replace(/\n/g, ' ')}</p>`).join('\n');
-    return md;
-}
-
-function markdownToAnsi(md) {
-    md = md.replace(/\*\*(.*?)\*\*/g, '\x1b[1m$1\x1b[0m');
-    md = md.replace(/_(.*?)_/g, '\x1b[3m$1\x1b[0m');
-    md = md.replace(/`(.*?)`/g, '\x1b[7m$1\x1b[0m');
-    md = md.replace(/```([\s\S]*?)```/g, (match, p1) => `\x1b[7m${p1}\x1b[0m`);
-    return md;
-}
+const { markdownToHtml, markdownToAnsi } = require('./regexp');
 
 const args = process.argv.slice(2);
 const formatIndex = args.indexOf('--format');
